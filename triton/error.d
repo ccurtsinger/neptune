76f6355327efe56f8e64995de3a93740e81566f3
Charlie Curtsinger <charlie@cs.umass.edu>
2007-10-23 06:23:10 +0000

    Forgot to halt exection on errors (will eventually throw an exception and attempt to handle it)
    git-svn-id: file:///Users/charlie/neptune/trunk@178 9f0c2d39-262c-0410-996f-cacf60396088

--- triton/error.d
+++ triton/error.d
@@ -1,22 +1,30 @@
 
 import std.stdio;
 
-extern (C) void _d_assert( char[] file, uint line )
+extern (C) void _d_assert(char[] file, uint line)
 {
     writefln("_d_assert(file: %s, line: %u)", file, line);
+    
+    for(;;){}
 }
 
-extern (C) static void _d_assert_msg( char[] msg, char[] file, uint line )
+extern (C) static void _d_assert_msg(char[] msg, char[] file, uint line)
 {
     writefln("_d_assert_msg(file: %s, line: %u)\n  %s", file, line, msg);
+    
+    for(;;){}
 }
 
-extern (C) void _d_array_bounds( char[] file, uint line )
+extern (C) void _d_array_bounds(char[] file, uint line)
 {
     writefln("_d_array_bounds(file: %s, line: %u)", file, line);
+    
+    for(;;){}
 }
 
-extern (C) void _d_switch_error( char[] file, uint line )
+extern (C) void _d_switch_error(char[] file, uint line)
 {
     writefln("_d_switch_error(file: %s, line: %u)", file, line);
+    
+    for(;;){}
 }

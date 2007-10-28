d538832d1383b4dc8914dffdce0f6616de4f39f3
Charlie Curtsinger <charlie@cs.umass.edu>
2007-10-28 23:01:09 +0000

    Modified candydoc script to generate more clearly-named tabs.  Refs #27
    git-svn-id: file:///Users/charlie/neptune/trunk@190 9f0c2d39-262c-0410-996f-cacf60396088

--- docs/candydoc/explorer.js
+++ docs/candydoc/explorer.js
@@ -3,7 +3,7 @@
 
    This file is javascript with classes that represents explorer window.
    And things related to navigation. */
-   
+
 var explorer = new Explorer();
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -15,57 +15,57 @@ function Marker()
     this.middle = document.createElement("div");
     this.bottom = document.createElement("div");
     this.container = document.createElement("div");
-    
+
     this.setTo = function(term)
     {
         // find definition related to `term`
         var def = term.nextSibling;
         while (def && def.nodeName != "DD")
             def = def.nextSibling;
-            
+
         var defHeight = 0;
         var childrenHeight = 0; // children of current declaration
         if (def)
         {
             defHeight = def.offsetHeight;
             var child = def.firstChild;
-            
+
             // traverse until DL tag, until children definition
             while (child && child.nodeName != "DL")
                 child = child.nextSibling;
-                
+
             if (child)
                 childrenHeight = child.offsetHeight;
         }
-        
+
         this.top.style.height = term.offsetHeight;
         this.middle.style.height = defHeight - childrenHeight;
         this.bottom.style.height = childrenHeight;
-        
+
         if (childrenHeight == 0)
             this.bottom.style.display = "none";
         else
             this.bottom.style.display = "";
-        
+
         this.container.style.left = getLeft(term) - 8;
         this.container.style.top = getTop(term);
         this.container.style.display = "";
     }
-        
+
     ///////////////////////////////////////////////////////////////////////////
     this.container.style.position = "absolute";
     this.container.style.display = "none";
-    
+
     this.top.className = "markertop";
     this.middle.className = "markermiddle";
     this.bottom.className = "markerbottom";
-    
+
     this.container.appendChild(this.top);
     this.container.appendChild(this.middle);
     this.container.appendChild(this.bottom);
-    
+
     //document.body.appendChild( this.container );
-    
+
     // Workaround bug in IE 5/6. We can not append anything to document body until
     // full page load.
     window.marker = this;
@@ -81,16 +81,16 @@ function Marker()
 function Outline()
 {
     this.tree           = new TreeView();
-    this.mountPoint     = null;    
+    this.mountPoint     = null;
     this.writeEnabled   = false;
-    this.marker         = new Marker();        
+    this.marker         = new Marker();
     this.classRegExp    = new RegExp;
     this.structRegExp   = new RegExp;
     this.enumRegExp     = new RegExp;
     this.templateRegExp = new RegExp;
     this.aliasRegExp    = new RegExp;
     this.funcRegExp     = new RegExp;
-    
+
     this.incSymbolLevel = function()
     {
         if (this.mountPoint == null)
@@ -106,20 +106,20 @@ function Outline()
         {
             child = this.mountPoint.children[i];
             var term = child.termRef;
-            
+
             // find first span node
             var n = term.firstChild;
             while (n && n.nodeName != "SPAN")
                 n = n.nextSibling;
-            
+
             if (!n) // shouldn't happen
                 continue;
-            
+
             var iconSrc;
             if (n.firstChild.nodeName == "#text")
             {
                 var text = n.firstChild.data; // text before declaration
-                
+
                 if ( this.classRegExp.test(text) )
                     iconSrc = "candydoc/img/outline/class.gif";
                 else if ( this.structRegExp.test(text) )
@@ -135,7 +135,7 @@ function Outline()
                     var np = n.firstChild;
                     while (np && np.nodeName != "SCRIPT") // find our script "onDecl"
                         np = np.nextSibling;
-                        
+
                     if (np && np.nextSibling && np.nextSibling.nodeName == "#text" &&
                         this.funcRegExp.test(np.nextSibling.data))
                     {
@@ -147,12 +147,12 @@ function Outline()
             }
             else // enum member ?
                 iconSrc = "candydoc/img/outline/var.gif";
-                    
+
             child.icon.src = iconSrc;
             child.icon.width = 16;
             child.icon.height = 16;
         }
-        
+
         this.mountPoint = this.mountPoint.parentNode;
     }
 
@@ -171,10 +171,10 @@ function Outline()
             var ret = getLastLeaf( document.getElementById("content") );
             while (ret && ret.nodeName != "DT")
                 ret = ret.parentNode;
-            
+
             return ret;
         }
-        
+
         if (this.writeEnabled)
         {
             var node = this.mountPoint.createChild(decl);
@@ -186,16 +186,16 @@ function Outline()
     this.mark = function(term)
     {
         this.marker.setTo(term);
-        window.scrollTo(0, getTop(term) - getWindowHeight() / 6);    
+        window.scrollTo(0, getTop(term) - getWindowHeight() / 6);
     }
-        
-    
+
+
     this.classRegExp.compile("(.*\b)?class(\b.*)?");
     this.structRegExp.compile("(.*\b)?struct(\b.*)?");
     this.enumRegExp.compile("(.*\b)?enum(\b.*)?");
     this.templateRegExp.compile("(.*\b)?template(\b.*)?");
     this.aliasRegExp.compile("(.*\b)?alias(\b.*)?");
-    this.funcRegExp.compile(/.*\(.*/);    
+    this.funcRegExp.compile(/.*\(.*/);
 }
 
 
@@ -206,8 +206,8 @@ function Outline()
 ///////////////////////////////////////////////////////////////////////////////
 function PackageExplorer()
 {
-    this.tree = new TreeView(true);    
-    
+    this.tree = new TreeView(true);
+
     this.addModule = function(mod)
     {
         var moduleIco = "candydoc/img/outline/module.gif";
@@ -217,14 +217,14 @@ function PackageExplorer()
         var node = this.tree.branch(path[0]);
         if ( !node )
             node = this.tree.createBranch(path[0], (path.length == 1) ? moduleIco : packageIco);
-        
+
         for (var i = 1; i < path.length; ++i)
         {
             var prev = node;
             node = node.child(path[i]);
             if (!node)
                 node = prev.createChild(path[i], (path.length == i + 1) ? moduleIco : packageIco);
-                
+
             if (path.length == i + 1)
                 node.setRef(path[i] + ".html");
         }
@@ -242,12 +242,12 @@ function Explorer()
     this.packageExplorer = new PackageExplorer();
     this.tabs            = new Array();
     this.tabCount        = 0;
-    
+
     this.initialize = function(moduleName)
     {
         this.tabArea = document.getElementById("tabarea");
         this.clientArea = document.getElementById("explorerclient");
-        
+
         // prevent text selection
         this.tabArea.onmousedown = new Function("return false;");
         this.tabArea.onclick = new Function("return true;");
@@ -255,23 +255,23 @@ function Explorer()
         this.clientArea.onmousedown = new Function("return false;");
         this.clientArea.onclick = new Function("return true;");
         this.clientArea.onselectstart = new Function("return false;");
-        
+
         this.outline.tree.createBranch( moduleName, "candydoc/img/outline/module.gif" );
-        
+
         // create tabs
-        this.createTab("Outline", this.outline.tree.domEntry);
-        this.createTab("Package", this.packageExplorer.tree.domEntry);
+        this.createTab("Packages", this.packageExplorer.tree.domEntry);
+        this.createTab("Current", this.outline.tree.domEntry);
     }
-    
+
     this.createTab = function(name, domEntry)
     {
         var tab = new Object();
         this.tabs[name] = tab;
         this.tabCount++;
-        
+
         tab.domEntry = domEntry;
         tab.labelSpan = document.createElement("span");
-        
+
         if (this.tabCount > 1)
         {
             tab.labelSpan.className = "inactivetab";
@@ -282,15 +282,15 @@ function Explorer()
             tab.labelSpan.className = "activetab";
             tab.domEntry.style.display = "";
         }
-        
+
         tab.labelSpan.appendChild( document.createTextNode(name) );
         tab.labelSpan.owner = this;
         tab.labelSpan.onclick = new Function("this.owner.setSelection('" + name + "');");
-        
+
         this.tabArea.appendChild( tab.labelSpan );
         this.clientArea.appendChild( domEntry );
     }
-    
+
     this.setSelection = function(tabName)
     {
         for (name in this.tabs)
@@ -298,7 +298,7 @@ function Explorer()
             this.tabs[name].labelSpan.className = "inactivetab";
             this.tabs[name].domEntry.style.display = "none";
         }
-        
+
         this.tabs[tabName].labelSpan.className = "activetab";
         this.tabs[tabName].domEntry.style.display = "";
     }

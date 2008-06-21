90da36dd6a8fa67aefb4d23239f073f70d99890c
Charlie Curtsinger <charlie@cs.umass.edu>
2008-06-21 19:03:11 +0000

    Added strpos and explode functions for parsing event domain strings
    git-svn-id: file:///Users/charlie/neptune/branches/simplification@438 9f0c2d39-262c-0410-996f-cacf60396088

--- kernel/runtime/std/string.d
+++ kernel/runtime/std/string.d
@@ -26,3 +26,38 @@ public size_t cstrlen(char* str)
     
     return i;
 }
+
+public int strpos(char[] haystack, char[] needle, size_t offset = 0)
+{
+    if(needle.length > haystack.length)
+        return -1;
+    
+    for(int i=offset; i<haystack.length - needle.length + 1 ; i++)
+    {
+        if(haystack[i..i+needle.length] == needle)
+            return i;
+    }
+    
+    return -1;
+}
+
+public char[][] explode(char[] delimiter, char[] str, size_t limit = 0)
+{
+    char[][] ret;
+    
+    size_t base = 0;
+    size_t top = 0;
+    
+    while(top < str.length)
+    {
+        top = strpos(str, delimiter, base);
+        
+        if(top == -1 || (limit != 0 && ret.length >= limit - 1))
+            top = str.length;
+            
+        ret ~= str[base..top];
+        base = top + 1;
+    }
+    
+    return ret;
+}

0f481acf97bef0f3506382de37086abedcaa1b91
Charlie Curtsinger <charlie@cs.umass.edu>
2008-06-25 21:20:54 +0000

    Purged old neptune development trunk
    git-svn-id: file:///Users/charlie/neptune/trunk@469 9f0c2d39-262c-0410-996f-cacf60396088

--- Makefile
+++
@@ -1,8 +0,0 @@
-all:
-	bash ./scons.sh neptune.iso
-
-cleanall:
-	bash ./scons.sh -c neptune.iso
-
-run:
-	bash ./run.sh
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-qemu-system-x86_64 -m 512 -cdrom neptune.iso -boot d -net nic,vlan=0 -net user,vlan=0  -localtime

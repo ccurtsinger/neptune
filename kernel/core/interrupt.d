75565f783ab77ecec4477a6098dc478544da15ad
Charlie Curtsinger <charlie@cs.umass.edu>
2008-08-06 22:41:25 +0000

    Converted the remaining interrupt handling code to inline assembly.
    git-svn-id: file:///Users/charlie/neptune/trunk@511 9f0c2d39-262c-0410-996f-cacf60396088

--- kernel/core/interrupt.d
+++ kernel/core/interrupt.d
@@ -134,6 +134,49 @@ struct InterruptScope
  	}
 }
 
+extern(C) void _isr_common_stub()
+{
+    asm
+    {
+        naked;
+        "push %%r15";
+        "push %%r14";
+        "push %%r13";
+        "push %%r12";
+        "push %%r11";
+        "push %%r10";
+        "push %%r9";
+        "push %%r8";
+        "push %%rdi";
+        "push %%rsi";
+        "push %%rdx";
+        "push %%rcx";
+        "push %%rbx";
+        "push %%rax";
+        "mov %%rbp, %%rdi";
+        "mov %%rsp, %%rsi";
+        "lea _common_interrupt(%%rip), %%rax";
+        "call *%%rax";
+        "pop %%rax";
+        "pop %%rbx";
+        "pop %%rcx";
+        "pop %%rdx";
+        "pop %%rsi";
+        "pop %%rdi";
+        "pop %%r8";
+        "pop %%r9";
+        "pop %%r10";
+        "pop %%r11";
+        "pop %%r12";
+        "pop %%r13";
+        "pop %%r14";
+        "pop %%r15";
+        "pop %%rbp";
+        "add $8, %%rsp";
+        "iretq";
+    }
+}
+
 extern(C) void _common_interrupt(ulong interrupt, Context* stack)
 {
     if(localscope.handlers[interrupt].set)
